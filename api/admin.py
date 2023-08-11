b0a7e52bc7fd031266b49b3ec2b71cf6e9e6b3d6
LauBeduneau <laurie.beduneau@gmail.com>
2023-08-11 15:04:58 +0200

    add filters on friendList and friendRequest for Admin

--- api/admin.py
+++ api/admin.py
@@ -4,6 +4,29 @@ from .models import Product, User, Category, FriendList, FriendRequest
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(User)
-admin.site.register(FriendList)
-admin.site.register(FriendRequest)
+
+
+class FriendListAdmin(admin.ModelAdmin):
+    list_filter = ['user']
+    list_display = ['user']
+    search_field = ['user']
+    readonly_field = ['user']
+
+    class Meta:
+        model = FriendList
+
+
+admin.site.register(FriendList, FriendListAdmin)
+
+
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_filter = ['sender', 'receiver']
+    list_display = ['sender', 'receiver']
+    search_field = ['sender__username', 'receiver__username']
+
+    class Meta:
+        model = FriendRequest
+
+
+admin.site.register(FriendRequest, FriendRequestAdmin)
 
