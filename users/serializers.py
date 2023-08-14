33161dddaee4dc2addbc0ceba3799e44515f1c82
LauBeduneau <laurie.beduneau@gmail.com>
2023-08-14 14:56:06 +0200

    fix user passwork

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,22 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ['id', 'is_superuser', 'date_joined', 'first_name',
                   'last_name', 'email', 'password', 'surname']
 
+    def create(self, validated_data):
+        # Hachage du mot de passe avant de créer l'utilisateur
+        password = validated_data.pop('password')
+        hashed_password = make_password(password)
+        user = User.objects.create(password=hashed_password, **validated_data)
+        return user
 
 class UserListSerializer(serializers.ModelSerializer):
     class Meta:
